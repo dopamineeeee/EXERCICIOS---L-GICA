974e5d2b15d9fe9b4bdd7b021453131d61bbf1bf
dopamineeeee <eric_oliveiraferreira@hotmail.com>
2026-02-27 23:24:42 -0300

    Refactor consumo.js with improved comments and formatting
    Updated comments for clarity and fixed string formatting for distance.

--- 6-consumo.js
+++ 6-consumo.js
@@ -6,7 +6,7 @@ gasolina: 5.8,
 
 alcool: 3.9,
 
-diesel: 6.20,
+diesel: 6.20,   // estava em string
 
 };
 
@@ -26,7 +26,7 @@ const viagem = {
 
 destino: "Litoral",
 
-distancia: 200,
+distancia: 200,  // estava em string
 
 veiculo: "Corsa",
 
@@ -34,9 +34,9 @@ veiculo: "Corsa",
 
 const resultadoGasolina = calcularGasto(viagem.distancia, 10, precos.gasolina);
 
-console.log("Viagem para o " + viagem.destino + " De " + viagem.veiculo);
+console.log("Viagem para o " + viagem.destino + " De " + viagem.veiculo); // adicionei para onde estava indo e com o modelo do carro
 
-console.log("Custo estimado no Diesel: R$ " + calcularGasto(200, 15, precos.diesel).toFixed(2));
+console.log("Custo estimado no Diesel: R$ " + calcularGasto(200, 15, precos.diesel).toFixed(2));  // adicionei o .tofized(2) para duas casas depois da virgula
 
 console.log("Custo estimado na Gasolina: R$ " + resultadoGasolina);
 
