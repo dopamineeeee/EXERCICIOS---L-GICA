9625ce264256c5e44accf27c94b9ef6bf4b6cc7b
dopamineeeee <eric_oliveiraferreira@hotmail.com>
2026-02-27 23:19:43 -0300

    Update 4-moeda.js

--- 4-moeda.js
+++ 4-moeda.js
@@ -2,7 +2,7 @@
 
 //  O objetivo deste código é simular um conversor de moedas simples, onde o usuário pode converter um valor em dólares para reais usando uma taxa de câmbio fixa. Identifique os erros presentes no código e corrija-os para que o conversor funcione corretamente.
 
-const taxaDolar = 5.50;
+const taxaDolar = 5.50;   // dolar estava em string mudei para número
 
 function converterParaReal(valorDolar) {
 
@@ -18,15 +18,15 @@ const transacao = {
 
 usuario: "Carlos",
 
-valor: 100.00,
+valor: 100.00, // valor estava em string mudei para número
 
 concluida: true,
 
 };
 
-let valorFinal = converterParaReal(transacao.valor);
+let valorFinal = converterParaReal(transacao.valor); // coloquei parametro transacao.valor
 
-if (transacao.concluida === true) {
+if (transacao.concluida === true) {  // coloquei ===
 
 console.log("O valor convertido para " + transacao.usuario + " é: " + valorFinal);
 
